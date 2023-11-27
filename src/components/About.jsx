af172c696639962715e20e6d441ec2376fb0b5d0
emmanuel badejo <emmanuelbadejoayodeji@gmail.com>
2023-11-27 18:56:45 +0100

    new about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className="py-16 flex items-center justify-center" id="about">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-center">
         <img src={Work} alt="Work" className="rounded w-[250px] h-[250px] items-center" />
-        <div className="md:ml-8 mt-4 md:mt-0 md:w-[500px] ">
+        <div className="md:ml-8 mt-4 md:mt-0 md:w-[500px] about-1" >
           <h4 className="text-3xl font-semibold">About Me</h4>
           <p className="text-sm mt-4">
           Experienced digital marketer with a two-year success record. Proficient in persuasive sales copy, targeted email marketing, and strategic social media ad campaigns. Specializing in React.js, TypeScript, and CSS frameworks like Tailwind and Bootstrap for crafting sleek, responsive websites.
